31b7f5f4cce2099a8e11e85c9f7614193208dad3
pritishg3 <ghate.pritish3@gmail.com>
2022-10-06 15:13:46 +0530

    Added code changes for sender

--- sender.js
+++ sender.js
@@ -1,43 +1,42 @@
-const { exec } = require('child_process');
 const fs = require('fs');
 const json = require('./assets/sensorData.json');
 
 
 function getTemperatureData() {
-    return json.temperature;
+  return json.temperature;
 }
 
 function getSocData() {
-    return json.soc;
+  return json.soc;
 }
 
 function writeToPipe(logger, data) {
-    logger.write(`\n`);
-    for(const num of data) {        
-        logger.write(`${String(num)}\t`);
-    }
+  logger.write(`\n`);
+  for (const num of data) {
+    logger.write(`${String(num)}\t`);
+  }
 }
 
 function updateTemperatureToPipe() {
-    var logger = fs.createWriteStream('temperaturelog.txt', {
-        flags: 'w'
-    })
-    writeToPipe(logger, getTemperatureData());
+  const logger = fs.createWriteStream('temperaturelog.txt', {
+    flags: 'w',
+  });
+  writeToPipe(logger, getTemperatureData());
 }
 
 function updateSocToPipe() {
-    var logger = fs.createWriteStream('soclog.txt', {
-        flags: 'w'
-    })
-    writeToPipe(logger, getSocData());
+  const logger = fs.createWriteStream('soclog.txt', {
+    flags: 'w',
+  });
+  writeToPipe(logger, getSocData());
 }
 
 updateTemperatureToPipe();
 updateSocToPipe();
 
 module.exports = {
-    getTemperatureData,
-    getSocData,
-    updateTemperatureToPipe,    
-    updateSocToPipe, 
-}
+  getTemperatureData,
+  getSocData,
+  updateTemperatureToPipe,
+  updateSocToPipe,
+};
--- test/sender.test.js
+++ test/sender.test.js
@@ -1,6 +1,5 @@
 
 const sender = require('../sender');
-const fs = require('fs');
 
 const chai = require('chai');
 const {expect} = require('chai');
@@ -8,19 +7,18 @@ const spies = require('chai-spies');
 chai.use(spies);
 
 describe('Sender', ()=>{
-    beforeEach(()=> {
-      console.log('Before');
+  beforeEach(()=> {
+    console.log('Before');
+  });
+  describe('Test getTemperatureData', () => {
+    it('Should retun temperature reading array', () => {
+      expect(sender.getTemperatureData()).to.deep.equal([20, 90, 80, 40, 50]);
     });
-   
-    describe('Test getTemperatureData', () => {
-        it('Should retun temperature reading array', () => {
-           expect(sender.getTemperatureData()).to.deep.equal([20,90,80,40,50]);
-        })
-    })
+  });
 
-    describe('Test getSocData', () => {
-        it('Should retun SOC reading array', () => {
-           expect(sender.getSocData()).to.deep.equal([20,30,40,50,60]);
-        })
-    })
-})
+  describe('Test getSocData', () => {
+    it('Should retun SOC reading array', () => {
+      expect(sender.getSocData()).to.deep.equal([20, 30, 40, 50, 60]);
+    });
+  });
+});
